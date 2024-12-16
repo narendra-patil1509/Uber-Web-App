d2b986c2113d0b1c5f858e5b8b71ea5257aa81d7
narendra-patil1509 <np738587@gmail.com>
2024-12-16 10:38:34 +0530

    Login session user security and captain security

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { UserDataContext } from './context/UserContext';
 import UserProtectWrapper from './pages/UserProtectWrapper';
 import UserLogout from './pages/UserLogout';
 import CaptainHome from './pages/CaptainHome';
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper';
 
 const App = () => {
 
@@ -34,7 +35,11 @@ const App = () => {
           <UserLogout/> 
         </UserProtectWrapper>
         } />
-        <Route path='/captain-home' element={<CaptainHome/>} ></Route>
+        <Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+            <CaptainHome/>
+          </CaptainProtectWrapper>
+        } ></Route>
       </Routes>
     </div>
   )
--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,19 +1,32 @@
 import React ,{ useState} from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { CaptainDataContext } from '../context/CaptainContext'
 import UberBlackLogo from '../assets/Uber_logo_2018.png'
 
 const CaptainLogin = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [captainData, setCaptaindata] = useState({});
+    const {captain, setCaptain} = React.useContext(CaptainDataContext);
+    const navigate = useNavigate();
 
-    const submitHandler = (e) => {
+    const submitHandler = async (e) => {
         e.preventDefault(); //to avoid default behaviour when we submiting form that is reloading when form submiting
-        setCaptaindata({
+        const captain = {
             email: email,
             password: password
-        })
+        }
+
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captains/login`,captain);
+
+        if(response.status === 200) {
+            const data = response.data;
+            setCaptain(data.captain);
+            localStorage.setItem('token',data.token);
+            navigate('/captain-home');
+        }
         setEmail('');
         setPassword('');
         console.log(captainData);
